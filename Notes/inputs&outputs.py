1d4c15ddf61b0874b5be0cec48febbd00b03455e
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-08-20 10:28:55 -0600

    Input Notes & Starting hello world

---
+++ Notes/inputs&outputs.py
@@ -0,0 +1,20 @@
+# TE 2nd Inputs & Outputs Notes
+
+# Input:
+print("Hello")
+print("")
+# An input could also be clicking the start button
+# Output shown below.
+
+# More info & examples:
+# Inputs need to be saved to a location
+# The keyword input is needed to create an input.
+# Then create the insturctions in the parenthesis next to the input
+color = input("Give me a color: ")
+print("So your favorite color is", color)
+
+# Other option
+print("")
+print("Tell me another color:")
+color_two = input()
+print(color_two, "is a nice color.")
