fbeada710eac0878ab6252c253202d4f5f01e471
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-08-21 10:51:01 -0600

    Varwiables

---
+++ Notes/variables.py
@@ -0,0 +1,17 @@
+# TE 2nd Variables
+
+#A variable must be named first, 
+# then we use our assignment operator to tell the computer what the variable’s value is, 
+# then we are going to set the content for this variable.
+print("")
+student_count = 29
+print(student_count)
+
+mcdonalds = "diabetes"
+print("")
+print(mcdonalds)
+print("")
+number_of_bigmacs = 6
+
+print(number_of_bigmacs-1)
+print("")
