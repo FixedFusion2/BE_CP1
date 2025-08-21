6d3bb4c73f54de395835a3401c371c389f0d5dc6
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-08-21 10:09:26 -0600

    Starter

---
+++ Starters/seasons.py
@@ -0,0 +1,9 @@
+# TE 2nd Seasons
+print("")
+print("Hello")
+print("")
+name = input("Whats your last name: ")
+print("")
+season = input("Whats your favorite season: ")
+print("")
+print("Wow", name, "san, I like", season, "too.")
