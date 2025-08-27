5a3b46d4b047254eac285b9a9e212f2136c6077c
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-08-27 10:50:40 -0600

    Notes

---
+++ Notes/numbers.py
@@ -0,0 +1,22 @@
+# TE 2nd Integers and Floats.
+
+#Integer
+age = 12345678909876432
+temp = 85
+attention = -8
+spoons = 4
+#An Integer is a whole number that can be negative or positive.
+money=-2
+money = 2
+
+#Float (Floating Point Number) any number with a decimal.
+
+pi = 3.14
+pounds = 5.6
+emotional_damage = .99
+patience_level = -.50
+
+#MATH!!!
+print("")
+print(8/4+7-(8+2)%4**2)                
+print("")
