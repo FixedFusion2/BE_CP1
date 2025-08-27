94ca4f3f74272ec89fb80ba2db5d558bb8ad48e3
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-08-27 10:25:20 -0600

    Changes

--- Practices/hello_admin.py
+++ Practices/hello_admin.py
@@ -6,6 +6,7 @@ admin = (" Ms. Larose", "True")
 
 #I can't quite remmebr how to make a list or an if then statement.
 
-if name == admin
+if name == ("Ms. Larose", "True")
     print("You are the cool one")
-else:
+
+else: print("Hello", name)
