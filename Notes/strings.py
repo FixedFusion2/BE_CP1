428e81f401f5f6e8aca76a3e8e621b8aab181ce4
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-09-02 10:51:15 -0600

    strings

--- Notes/strings.py
+++ Notes/strings.py
@@ -1,3 +1,31 @@
 #TE 2nd Strings
 
-str
+#string = str
+#str = "content"
+
+numbers = "1,2,3,4,5"
+print(str(3475456))
+sentence  = "This is a sentence."
+candy = "yummy"
+mcdonalds = "Diabetes"
+diabetes = "death"\
+
+mcdonalds = "death"
+#Double quotes = start and end, apostrophes are on the inside.
+name = "true' egg"
+month = "september"
+lentgh = len(month)
+print(lentgh)
+
+#Escape Charcter
+print("\nWill said: Have a great day!")
+
+#Concatenate
+string_1 = "cool"
+string_2 = "awesome"
+print(string_1 + " " + string_2)
+
+# String[start index, end index]
+
+#  123456789
+# "indexing is cool"
