a582f548f5aac45a49b3922e78b29a5b2e44c081
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-09-03 10:49:30 -0600

    mad lib start

---
+++ Practices/mad_lib.py
@@ -0,0 +1,30 @@
+# TE 2nd Mad Lib
+
+adjective = input("Give me an adjective: ")
+noun = input("Give me a noun: ")
+plural_noun = input("Give me a plural noun: ")
+aunt = input("Give mea a person in room (female): ")
+adjective_2 =input("Give me another adjective: ")
+clothing = input("Give me an article of clothing: ")
+noun_2 = input("Give me another noun: ")
+city = input("Give me a city: ")
+plural_noun_2 = input("Give me another plural noun: ")
+adjective_3 = input("Give me another adjective: ")
+part_of_body = input("Give me a part of the body: ")
+letter = input("Give me a letter of the alphabet: ")
+celebtrity = input("Give me a celebrity: ")
+plural_noun_3 = input("Give me another plural noun: ")
+adjective_4 = input("Give me another adjective: ")
+place = input('Give me a place:')
+part_of_body_2 = input("Give me another part of the body:")
+adjective_5 = input("Give me another adjective: ")
+adjective_6 = input("Give me another adjective: ")
+verb = input("give me a verb:")
+plural_noun_4 = input("Give me another plural noun:")
+number  = input("Give me a number:")
+
+print("There are many"+adjective+' ways to choose a/an '+noun+'. To read. '
+'First, you could ask for recommendations from your friends and'+plural_noun+'. Just do not ask'+aunt+', she only reads'
++adjective_2+' books with'+clothing+'ripping goddesses on the cover. If your friends and family are no help, try checking out the'
++noun_2+'Review in The'+city+'Times. If the'+plural_noun_2+' featured there are too '+adjective_3+'for your taste, try something a little'
+''
