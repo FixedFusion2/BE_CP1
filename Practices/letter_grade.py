3e8ed4354d9f00716d843ac67879dc059715d27b
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-09-24 10:23:27 -0600

    Letter Grade

--- Practices/letter_grade.py
+++ Practices/letter_grade.py
@@ -14,33 +14,31 @@ print("")
 grade = float(input("Enter your grade percent(no percent symbol): "))
 
 
-if grade >= 100:
+if grade > 100:
     slow_print("You are an overachiever.")
-elif grade >= 93:
+elif grade > 93 and grade < 101:
     print("You have an A")
-elif grade >= 90:
+elif grade >= 91:
     print("You have a B+")
 elif grade == 90:
     print("You have a B")
-elif grade <=90:
+elif grade < 90 and grade >=80:
     print("You have a B-")
-elif grade >=70:
+elif grade < 76 and grade > 70:
     print("You have a C+")
 elif grade == 70:
     print("You have a C")
-elif grade <=70:
+elif grade < 70 and grade > 65:
     print("You have a C-")
-elif grade >= 60:
+elif grade > 60 and grade < 64:
     print("You have a D+")
-elif grade == 60:
+elif grade > 55 and grade < 51:
     print("You have a D")
-elif grade >= 60:
-    print("You have a D-")
-elif grade >= 50:
-    print("You have an F+")
 elif grade == 50:
+    print("You have a D-")
+elif grade < 50 and grade > 45:
     print("You have an F")
-elif grade <= 50:
+elif grade <= 44:
     slow_print("How? HOW? how...*sigh*...")
 else:
-    print("Numeric values only excepted nimrod.")
+    print("Numeric values only, ya nimrod.")
