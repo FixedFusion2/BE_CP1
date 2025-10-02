ca3351f53f8cd833b9462c6fcc4b87b3f1f1cb01
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-10-02 10:49:12 -0600

    update

--- Practices/nultiplication_table.py
+++ Practices/nultiplication_table.py
@@ -1,7 +1,7 @@
 # TE 2nd Multiplication Table
 
 nums = int(input('Please enter a positive integer between 1 an 30: '))
-for row in range(1,nums + 1):
-    for col in range(1,nums + 1):
-        print(row*col, end = "\t")
+for row in range(1, nums + 1):
+    for col in range(1, nums + 1):
+        print(row * col, end = "\t")
     print()
