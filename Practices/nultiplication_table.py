2fe49f61becd36ed9ff838ff0405774f3dc65372
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-10-03 09:59:28 -0600

    s

--- Practices/nultiplication_table.py
+++ Practices/nultiplication_table.py
@@ -1,5 +1,13 @@
 # TE 2nd Multiplication Table
+import time
+def slow_print(text, delay=0.05):
+    """Print text slowly, character by character"""
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
 
+
+slow_print("\nMultiplication Table Opened\n")
 nums = int(input('Please enter a positive integer between 1 an 30: '))
 for row in range(1, nums + 1):
     for col in range(1, nums + 1):
