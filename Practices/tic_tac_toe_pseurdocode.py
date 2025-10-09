0357b482d7cd2a950d14bfcccdb4ea8e29b041f7
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-10-09 10:44:37 -0600

    PSEUDOCODE TIC TAC TOE!!!

---
+++ Practices/tic_tac_toe_pseurdocode.py
@@ -0,0 +1,40 @@
+# TE 2nd Tic-Tac-Toe in Pseudocode.
+
+# import random for a random number generator to select a box to put an O in.
+
+# Loop to loop the game if the user wants to keep going:
+    # Board is also a list with the numbers
+    # Board is displayed as [1],[2],[3] one line down [4],[5],[6]one line down [7],[8],[9]
+    #Loop for player an computer turn:
+        # Computer variable is set to a random integer between 1 & 9
+        # Player variable is set to an input where the player chooses a number on the gameboard
+        # If player selects a number in game board change it to O
+        # If computer selects number in game board change it to X 
+    
+    # If player gets [1],[2],[3] then thy win
+    # If player gets [4],[5],[6] then they win
+    # If player gets [7],[8],[9] then they win
+    
+    # If player gets [1],[5],[9] then they win
+    # If player gets [7],[5],[3] then they win
+    
+    # If player gets [1],[4],[7] then they win
+    # If player gets [2],[5],[8] then they win
+    # If player gets [3],[6],[9] then they win
+
+    # If computer gets [1],[2],[3] then thy win
+    # If computer gets [4],[5],[6] then they win
+    # If comuter gets [7],[8],[9] then they win
+    
+    # If computer gets [1],[5],[9] then they win
+    # If computer gets [7],[5],[3] then they win
+    
+    # If computer gets [1],[4],[7] then they win
+    # If computer gets [2],[5],[8] then they win
+    # If computer gets [3],[6],[9] then they win
+    # If this has looped 3 times then break while loop and ask player if they want to exit or keep going.
+
+
+
+
+    
