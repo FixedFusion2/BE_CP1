e7ff256e56f7569a7c5a16383aa894c242b3986d
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-10-22 10:16:56 -0600

    cipher

--- Practices/caesar_cipher.py
+++ Practices/caesar_cipher.py
@@ -2,7 +2,7 @@
 
 #Function that lets you insert message, shift, or change the mode from encode to decode.
 
-def cipher(text, shift, mode):
+def cipher(text, shift, mode):#Parameters of the function for the messsage, the shift, and the mode (Mode is kinda a placeholder for choice).
     result = ""
     #For loop to go through each character one by one
     for char in text: 
@@ -13,25 +13,15 @@ def cipher(text, shift, mode):
             base = ord("a")#Lower case a
             result += chr((ord(char)- base + shift)%26 + base)#Unicode, 65, modula for wraping around the alphabet
         else:
-            result += char
+            result += char#For other values
     return result
 
 
-print("\nCaesar Cipher Machine opened.")
-choice = input(" \nType 'encode' to encrypt, type 'decode' to decrypt: ")#Inputs for user
-message = input("\nEnter your message (only letter values): ")
-shift = int(input("\nEnter how many letters do you want to shift: "))
+print("\nCaesar Cipher opened.")
+choice = input(" \nType 'encode' to encrypt, type 'decode' to decrypt: ")#Input for user that asks them if they want to encrypt or decrpyt.
+message = input("\nEnter your message (only letter values): ")#Input for message
+shift = int(input("\nEnter how many letters do you want to shift: "))#Input for How many letter they want to shift.
 
 if choice == 'decode':#For decoding
     shift = -shift
-print(cipher(message , shift, choice)) 
-
-
-    
-
-
-
-
-
-
-
+print(cipher(message , shift, choice)) #The output of the code using the parameters of the function.
