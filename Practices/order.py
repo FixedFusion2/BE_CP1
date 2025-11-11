a4b19135b8be5e41224efc48e87eacfa5756570f
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-11-11 10:50:24 -0700

    order

--- Practices/order.py
+++ Practices/order.py
@@ -4,8 +4,25 @@
 # Users need to be able to order a drink, a main course and two side dishes. 
 # At the end the program needs to repeat back to the user their full order, and the total cost. 
 
-menu = {"Drinks": {"Rootbeer": 2, "Sprite": 2, "Water": "Free", "Coke": 2, "Pepsi": "Don't buy this crap you idiot", "Dr. Pepper": 2}, 
-        "Entree": {"Hamburger": 2, "Cheeseburger": 5, "Salad": 4, "Sandwhich": 5}, 
-        "Sidedishes": {"Rosemary Fries": 3, "Chips": 3, "Fruit Cup": 3}}
+menu = {"Drinks": {"Rootbeer": 2, "Sprite": 2, "Water": 0, "Coke": 2, "Dr. Pepper": 2}, 
+        "Entree": {"Hamburger": 5, "Cheeseburger": 5, "Chicken Salad": 4, "Sandwich": 5}, 
+        "Sidedishes": {"Rosemary Fries": 3, "Chips": 2, "Fruit Cup": 3}}
 
-print("Welcome to Aunt Rosey's 1967 Diner!")
+print("Welcome to Aunt Rosey's Diner!")
+
+print("\nMenu:\n")
+print("Entrees: Hamburger: $5, Cheeseburger: $5, Chicken Salad: $4, Sandwich $5\n")
+print("Sidedishes: Rosemary Fries: $3, Chips: $2, Fruit Cup: $3\n")
+print("Drinks: Rootbeer: $2, Sprite: $2, Water: Free, Coke: $2, Dr. Pepper $2\n")
+
+entree = input("Choose your entree (Type: Hamburger, Cheeseburger, Chicken Salad or Sandwhich): ")
+sidedish1 = input("Choose one side dish (Type: Rosemary Fries, Chips, or Fruit Cup): ")
+sidedish2 = input("Choose another sidedish: ")
+drink = input("Choose a drink (Type: Rootbeer, Sprite, Water, Coke, or Dr. Pepper): ")
+
+print(f"The cost of the Entree is {menu["Entree"][entree]} Dollars")
+print(f"The cost of the First Dish is {menu["Sidedishes"][sidedish1]} Dollars")
+print(f"The cost of the Second Dish is {menu["Sidedishes"][sidedish2]} Dollars")
+print(f"The cost of the Drink is {menu["Drinks"][drink]} Dollars")
+
+print(f" {menu["Entree"][entree]} + {menu["Sidedishes"][sidedish1]} + {menu["Sidedishes"][sidedish2]} + {menu["Drinks"][drink]} = ")
