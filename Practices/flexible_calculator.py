74c45d47931c623a7f40076f98dd18894c35b67a
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-11-19 03:22:10 +0000

    Flex Calc

--- Practices/flexible_calculator.py
+++ Practices/flexible_calculator.py
@@ -2,34 +2,51 @@
 
 #Function fo calculation
 # uses *args to accept any number of values (numbers). The function should also accept an optional parameter for the operation to be performed.
-def calculate():
+def calculate(*args, operation = "sum"):#Parameters for args and operation.
 #If statement for operation being set to sum
     if operation == "sum":
-        print(sum(numbers))
+        print(sum(args))
         #If statement for operation being set to average 
-    if operation == "average":
-        print(sum(numbers)/len(numbers))
+    elif operation == "average":
+        print(sum(args)/len(args))
         #If statement for operation being set to max 
-    if operation == "max":
-        print(max(numbers))
+    elif operation == "max":
+        print(max(args))
         #If statement for operation being set to min
-    if operation == "min":
-        print(min(numbers))
+    elif operation == "min":
+        print(min(args))
         #If statement for operation being set to product
-    if operation == "product":
-        print(numbers * numbers)
+    elif operation == "product":
+        product = 1
+        for num in args:
+            product *= num
+        print(product)
 
 
-#Welcome to the Flexible Calculator!
-print("Welcome to Flexible Calculator")
-#Available operations: sum, average, max, min, product
-print("Availabe Operations: sum, average, max, min, product")
-#Which operation would you like to perform?
-operation = input("What operation would you like to perform: ")
-#While loop for numbers 
+#While True to keep calculating
 while True:
-    numbers = str(input("Enter your numbers (type done when finished): "))
-    if numbers == "done":#If the user types "done" break the loop
+
+    #Number list to hold number values from user input
+    numbers_list = []
+    #Welcome to the Flexible Calculator!
+    print("Welcome to Flexible Calculator")
+    #Available operations: sum, average, max, min, product
+    print("Availabe Operations: sum, average, max, min, product")
+    #Which operation would you like to perform?
+    operation = input("What operation would you like to perform: ")
+    #While loop for numbers 
+    while True:
+        num_entry = input("Enter your numbers (type done when finished.): ")
+        if num_entry == "done":#If the user types "done" break the loop
+            break
+        numbers_list.append(float(num_entry))
+
+    #Call function
+    calculate(*numbers_list, operation=operation)
+    #Let the user decide if they want to keep calculating or not.
+    again = input("Would you like to keep calculating? (Type yes or no): ")
+    if again == "no":
+        print("Thanks for using the calculator.")
         break
-calculate()
+
 
