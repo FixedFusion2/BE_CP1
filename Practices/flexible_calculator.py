16a2c4e11d6f732088c4022a834eb3f84c8717f9
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-11-19 03:25:52 +0000

    Felx Calc more pseudo

--- Practices/flexible_calculator.py
+++ Practices/flexible_calculator.py
@@ -1,26 +1,26 @@
 # TE 2nd Flexible Calculator
 
 #Function fo calculation
-# uses *args to accept any number of values (numbers). The function should also accept an optional parameter for the operation to be performed.
+# uses *args to accept any number of values. Operation for operation.
 def calculate(*args, operation = "sum"):#Parameters for args and operation.
 #If statement for operation being set to sum
     if operation == "sum":
-        print(sum(args))
+        print(sum(args))#Showing the output
         #If statement for operation being set to average 
     elif operation == "average":
-        print(sum(args)/len(args))
+        print(sum(args)/len(args))#Showing the output
         #If statement for operation being set to max 
     elif operation == "max":
-        print(max(args))
+        print(max(args))#Showing the output
         #If statement for operation being set to min
     elif operation == "min":
-        print(min(args))
+        print(min(args))#Showing the output
         #If statement for operation being set to product
     elif operation == "product":
-        product = 1
+        product = 1#variable for product to start
         for num in args:
             product *= num
-        print(product)
+        print(product)#Showing the output
 
 
 #While True to keep calculating
@@ -44,9 +44,9 @@ while True:
     #Call function
     calculate(*numbers_list, operation=operation)
     #Let the user decide if they want to keep calculating or not.
-    again = input("Would you like to keep calculating? (Type yes or no): ")
-    if again == "no":
-        print("Thanks for using the calculator.")
+    again = input("Would you like to keep calculating? (Type yes or no): ")#Again variable for checking if they want to keep calculating.
+    if again == "no":#If no break while loop
+        print("Thanks for using the calculator.")#Showing the output
         break
 
 
