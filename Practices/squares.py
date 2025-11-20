5710743700aed358666371965049db00cce587a5
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-11-20 10:08:31 -0700

    sad

--- Practices/squares.py
+++ Practices/squares.py
@@ -4,7 +4,6 @@
 #Note: You can use this list or make your own list of numbers but your list should have at least 20 items in it.
 
 numbers = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]#Numbers list
-
 squared = list(map(lambda num: num**2, numbers))#Squared variable with lambda function.
 for i in range(20):
     print(f"Original: {numbers[i]}, Squared: {squared[i]}")#Prints 1-20.
