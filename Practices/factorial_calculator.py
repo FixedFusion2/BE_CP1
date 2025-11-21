01af350850bc1c9ac895b1537719539b85e5755d
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-11-21 10:47:03 -0700

    factorial calculator.

---
+++ Practices/factorial_calculator.py
@@ -0,0 +1,33 @@
+# TE 2nd Factorial Calculator
+
+#Define function getting INP
+def getting_INP():
+    #If INP can't be turned into float, return False
+    if input is float:
+        return False
+    #If float INP-integers INP is not equal to 0, return Flase
+    if input is int = 0:
+        return False
+    #Else return false
+    else:
+        return True
+#Def function factorial getting NUM
+def factorial_getting_NUM():
+    #Set DON to 1
+    done = 1
+    #Loop over list stating with NUM, counting down by on 1
+        #Set DON to DON multiplied by list item(current),
+        done =
+    #Return DON
+
+#Set IN to user input of "What number would you like to factorial?"
+factorial = input("What number would you like to factorial: ")
+#Loop while function check on IN is FALSE
+while getting_INP == False:
+    #Display invalid user input
+    print("Invalid User Input")
+    #Set IN to user input of "What number would you like to factorial?"
+    factorial = input("What number would you like to factorial: ")
+#display (IN) factorial is equal to (function factorial on integers IN)
+
+getting_INP() = factorial_getting_NUM()
