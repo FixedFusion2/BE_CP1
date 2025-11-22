b8e1d1f4329bda12a21b4b45985977a4d6191bf2
FixedFusion2 <true.eggleston@ucas-edu.net>
2025-11-22 00:02:29 +0000

    Factorial

--- Practices/factorial_calculator.py
+++ Practices/factorial_calculator.py
@@ -1,33 +1,41 @@
 # TE 2nd Factorial Calculator
 
 #Define function getting INP
-def getting_INP():
+def getting_INP(user_input):
+    try:
+        num =float(user_input)
+    except:
     #If INP can't be turned into float, return False
-    if input is float:
         return False
     #If float INP-integers INP is not equal to 0, return Flase
-    if input is int == 0:
+    if num - int(num) != 0:
         return False
-    #Else return false
-    else:
-        return True
+    
+    #If the integers is less than 0, return false
+    if int(num) < 0:
+        return False
+    #Else return True
+    return True
 #Def function factorial getting NUM
-def factorial_getting_NUM():
+def factorial_getting_NUM(num):
     #Set DON to 1
     done = 1
     #Loop over list stating with NUM, counting down by on 1
+    for i in range(num, 0, -1):
         #Set DON to DON multiplied by list item(current),
-        done =
+        done = done * i
     #Return DON
     return done
 #Set IN to user input of "What number would you like to factorial?"
 factorial = input("What number would you like to factorial: ")
 #Loop while function check on IN is FALSE
-while getting_INP == False:
+while getting_INP(factorial) == False:
     #Display invalid user input
     print("Invalid User Input")
     #Set IN to user input of "What number would you like to factorial?"
     factorial = input("What number would you like to factorial: ")
-#display (IN) factorial is equal to (function factorial on integers IN)
+#display factorial is equal to integers
+factorial = int(factorial)
 
-getting_INP() = factorial_getting_NUM()
+#Display result
+print(f"{factorial}! is equal to {factorial_getting_NUM(factorial)} ")
